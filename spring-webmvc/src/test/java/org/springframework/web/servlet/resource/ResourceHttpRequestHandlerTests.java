0270f00b8c7c010073f48bd12d799a11dd36d2a2
rstoyanchev <rossen.stoyanchev@broadcom.com>
2024-10-14 05:55:18 -0600

    Normalize static resource path early
    Rather than leaving it to the Resource implementation, and
    potentially normalizing twice, we apply it once as part of the
    initial processPath checks.
    
    Closes gh-33689
    
    (cherry picked from commit 3bfbe30a7814c9ea1556d40df9bd87ddb3ba372d)

--- spring-webmvc/src/test/java/org/springframework/web/servlet/resource/ResourceHttpRequestHandlerTests.java
+++ spring-webmvc/src/test/java/org/springframework/web/servlet/resource/ResourceHttpRequestHandlerTests.java
@@ -362,7 +362,6 @@ public class ResourceHttpRequestHandlerTests {
 		testInvalidPath("/../.." + secretPath, handler);
 		testInvalidPath("/%2E%2E/testsecret/secret.txt", handler);
 		testInvalidPath("/%2E%2E/testsecret/secret.txt", handler);
-		testInvalidPath("%2F%2F%2E%2E%2F%2F%2E%2E" + secretPath, handler);
 	}
 
 	private void testInvalidPath(String requestPath, ResourceHttpRequestHandler handler) throws Exception {
@@ -742,6 +741,8 @@ public class ResourceHttpRequestHandlerTests {
 		assertThat(this.response.getContentAsString()).isEqualTo("h1 { color:red; }");
 	}
 
+
+
 	@Test
 	public void servletContextRootValidation() {
 		StaticWebApplicationContext context = new StaticWebApplicationContext() {
@@ -762,7 +763,7 @@ public class ResourceHttpRequestHandlerTests {
 	}
 
 
-	private long resourceLastModified(String resourceName) throws IOException {
+		private long resourceLastModified(String resourceName) throws IOException {
 		return new ClassPathResource(resourceName, getClass()).getFile().lastModified();
 	}
 
